7843fe11e6fcaac0ba02041fef8a16d9cdce4749
Mahmoud Alismail <mahmoud.alismail@gmail.com>
2014-12-07 00:13:55 +0300

    implemented WitClient

---
+++ server/WitClient.py
@@ -0,0 +1,20 @@
+import wit 
+import json
+
+class WitClient(object):
+	"""docstring for WitClient"""
+	_access_token = 'NBPOVLY7T6W3KOUEML2GXOWODH3LPWPD'
+
+	def __init__(self):
+		wit.init()
+
+	def text_query(self, text):
+		res = json.loads(wit.text_query(text, WitClient._access_token))
+		return res["outcomes"]
+		
+	def close_connection(self):
+		wit.close()
+
+if __name__ == "__main__":
+	
+	print "You ran the Wit client, nothing will happen. Exiting..."
