236e4b2e420dcc4cddc9644f43c81c21b912703d
Mahmoud Alismail <mahmoud.alismail@gmail.com>
2014-12-06 20:24:00 +0300

    backend communication with frontend

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bottle import route, run, get, request, Bottle
+from bottle import route, run, get, request, Bottle, redirect
 from FirebaseClient import *
 from imojify import imojify_app
 
@@ -10,17 +10,36 @@ root.mount('imojify', imojify_app)
 
 @root.get('/')
 def server_root():
-	return "WELCOME to jitalk"
+    return "WELCOME to jitalk"
 
 @root.get('/firebase')
 def firebase_client():
-	# check the FirebaseClient class 
+    # check the FirebaseClient class 
 
-	firebaseInstance = FirebaseClient()
-	
-	# print firebaseInstance.postChat("-JcUVyYt9eBbWTWrw64V", "Mahmoud", "Order matters")
-	# roomID = firebaseInstance.createRoom();
+    firebaseInstance = FirebaseClient()
+    
+    # print firebaseInstance.postChat("-JcUVyYt9eBbWTWrw64V", "Mahmoud", "Order matters")
+    # roomID = firebaseInstance.createRoom();
 
-	return None
+    return None
+
+@root.get('/createRoom')
+def create_room():
+
+    firebaseInstance = FirebaseClient()
+    roomID = firebaseInstance.createRoom()
+    redirect('/room/'+roomID)
+
+@root.get('/room/<roomID>')
+def chat_room(roomID):
+    # you can share the room so others can join you
+    # embed the roomID into the return page
+    # return page
+    return roomID
+
+@root.get('/sendMessage')
+def post_message():
+    jsonData = json.load(request.json)
+    #jsonData.data = {roomID: "", username: "", text: ""}
 
 root.run(host='0.0.0.0', port=8080, debug=True)
