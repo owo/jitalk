e2616d9b8a59182cbcde0b439fc26000af68bee4
Afnan Fahim <afahim@groupon.com>
2014-12-07 06:32:39 -0600

    rudolph the ******* reindeer

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,10 +1,30 @@
-//The name of blue user
-var blueUser = "";
+/*Global Variables*/
+var messagesRef;
+var roomID;
 
-//The name of the white user
-var whiteUser = "";
+var otherUser = "";
+
+var thisUser = "";
+
+function setupFirebaseHandlers() {
+
+  messagesRef.limitToLast(10).on('child_added', function (snapshot) {
+    var data = snapshot.val();
+    var username = data.name;
+    var images = data.text;
+
+    displayNewChat(username, images);
+  });
+}
 
 $( document ).ready(function() {
+  roomID = $(".roomID").text() || 123456 
+  messagesRef = new Firebase('https://ji-talk.firebaseio.com/'+roomID);
+
+  setupFirebaseHandlers();
+
+  thisUser = $(".username").text();
+
   $(document).keypress(function(event) {
     if (event.keyCode == 13) {
         event.preventDefault();
@@ -22,26 +42,14 @@ $( document ).ready(function() {
 });
 
 function sendChatMsg() {
-    $(".submit").css("-webkit-animation", "circle-of-life 0.5s ease-in");
-    var inputString = getAndResetInputField();
-    //ToDo: send input string to server here
+  $(".submit").css("-webkit-animation", "circle-of-life 0.5s ease-in");
+  sendToServer(getAndResetInputField());
 
-
-    //Creating chat for testing
-    blueUser = "afnan"
-    whiteUser = "mahmoud"
-
-    var selectBlueUser = Math.random()<.5; 
-    var sender = selectBlueUser ? "afnan" : "mahmoud";
-
-    var imageUrls = ["http://emojipedia.org/wp-content/uploads/2013/07/160x160x78-cat-face-with-wry-smile.png.pagespeed.ic.1ygkM3ku-M.jpg",
-                     "http://emojipedia.org/wp-content/uploads/2014/04/128x128x1f63c-google-android.png.pagespeed.ic.Seerrsa2qB.png", 
-                     "http://emojipedia.org/wp-content/uploads/2014/04/72x72x1f63c-twitter.png.pagespeed.ic.utvqmVWtXn.png"];
-    displayNewChat(sender, imageUrls);
+  generateDummyData();
 }
 
 function displayNewChat(senderName, imgUrls) {
-  var userClass = (whiteUser == senderName) ? "white" : "blue";
+  var userClass = (thisUser == senderName) ? "this-user" : "other-user";
 
   var sentence = document.createElement("div");
   sentence.className = "sentence hidden-chat " + userClass;
@@ -60,6 +68,25 @@ function displayNewChat(senderName, imgUrls) {
   }
 }
 
+function sendToServer(string) {
+  $.ajax({
+    type: 'GET', // or POST
+      url: '/sendMessage',
+      contentType: "application/json",
+      data: {
+          "roomID": roomID,
+          "username": thisUser,
+          "text": string
+      },
+      success: function (data) {
+          console.log("Message sent!")
+      },
+      error: function (e) {
+          console.log("ERROR", e.message)
+      }
+  })
+}
+
 function getAndResetInputField() {
   var input = $(".input-field").text();
   $(".input-field").text("");
@@ -72,5 +99,20 @@ function scrollToBottom() {
 
 function scaleImg(img){
   img.style.height = "50px";
+}
+
+function generateDummyData() {
+  //Creating chat for testing
+  otherUser = "afahim"
+  thisUser = "mahmoud"
+
+  var selectOtherUser = Math.random()<.5; 
+  var sender = selectOtherUser ? "afahim" : "mahmoud";
+
+  var imageUrls = ["http://emojipedia.org/wp-content/uploads/2013/07/160x160x78-cat-face-with-wry-smile.png.pagespeed.ic.1ygkM3ku-M.jpg",
+                   "http://emojipedia.org/wp-content/uploads/2014/04/128x128x1f63c-google-android.png.pagespeed.ic.Seerrsa2qB.png", 
+                   "http://emojipedia.org/wp-content/uploads/2014/04/72x72x1f63c-twitter.png.pagespeed.ic.utvqmVWtXn.png"];
+
+  displayNewChat(sender, imageUrls);
 }
 
