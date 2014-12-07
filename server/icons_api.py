fdb17a3e099d701442353a408c9a9d8e78ef1085
Ossama W. Obeid <owo@owobeid.com>
2014-12-07 21:50:51 +0300

    Added nounproject support to imojify. Holy shit Ziadgit add imojify.py icons_api.pygit add imojify.py icons_api.py

--- server/icons_api.py
+++ server/icons_api.py
@@ -6,25 +6,24 @@ import json
 from googleapiclient.discovery import build
 from requests_oauthlib import OAuth1
 
+
 # EXPECTED: developer_key, cx
 def img_query(queryText, secrets):
-  
-  
-  service = build("customsearch", "v1",
-            developerKey=secrets['developer_key'])
+    service = build("customsearch", "v1",
+              developerKey=secrets['developer_key'])
 
-  res = service.cse().list(
-      q=queryText,
-      cx=secrets['cx'],
-      imgSize="medium",
-      searchType="image"
+    res = service.cse().list(
+        q=queryText,
+        cx=secrets['cx'],
+        imgSize="medium",
+        searchType="image"
     ).execute()
 
-  return res
+    return res
+
 
 # parse google image search query response
 def parse_img_query(unparsed):
-
     if unparsed != None:
         links = unparsed['items']
         firstImage = links[0]['image']
@@ -33,9 +32,18 @@ def parse_img_query(unparsed):
     else:
         return None
 
+
+def parse_nounproject(unparsed):
+    if "icons" in unparsed.keys():
+        icons = unparsed['icons']
+        firstIcon = icons[0]['icon_url']
+        return firstIcon
+    else:
+        return None
+
+
 # EXPECTED: public_key, secret_key
 def nounproject_query(queryText, secrets):
-
     public_key = secrets['public_key'] 
     secret_key = secrets['secret_key'] 
 
@@ -43,16 +51,14 @@ def nounproject_query(queryText, secrets):
     endpoint = "http://api.thenounproject.com/icons/"+queryText+"?limit_to_public_domain=1&limit=5"
     res = requests.get(endpoint, auth=auth)
     
-    return json.loads(res.content)
+    resp = None
 
-def parse_nounproject(unparsed):
+    try:
+        resp = parse_nounproject(json.loads(res.content))
+    except:
+        pass
 
-    if "icons" in unparsed.keys():
-        icons = unparsed['icons']
-        firstIcon = icons[0]['icon_url']
-        return firstIcon
-    else:
-        return None
+    return resp
 
 
 # google image_query, handle with care!
